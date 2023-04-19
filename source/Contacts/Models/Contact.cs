02053ec941c1902d01a62f1af6842fbfb15069d0
Alexey Gast <ulitracat@gmail.com>
2023-04-19 13:20:29 +0700

    lab 1 looks done to me + moved C++ folder where it should been from the bigining

---
+++ source/Contacts/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿namespace Contacts.Models;
+
+/// <summary>
+/// Представляет контакт.
+/// </summary>
+public class Contact
+{
+    /// <summary>
+    /// Возвращает или задает значение имени контакта.
+    /// </summary>
+    public string Name { get; set; } = "";
+
+    /// <summary>
+    /// Возвращает или задает значение номер телефона.
+    /// </summary>
+    public string PhoneNumber { get; set; } = "";
+
+    /// <summary>
+    /// Возвращает или задает значение адрес электронной почты.
+    /// </summary>
+    public string Email { get; set; } = "";
+}
